3b4db9f3dad738dfbaf114fa7c59cf7ff5f043d1
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-14 17:24:39 +0500

    CurrencyService.java: added getExchangeRates() method

--- src/main/java/currency_exchange_api/service/CurrencyService.java
+++ src/main/java/currency_exchange_api/service/CurrencyService.java
@@ -1,9 +1,12 @@
 package currency_exchange_api.service;
 
 import currency_exchange_api.model.Currency;
+import currency_exchange_api.model.ExchangeRate;
 import java.util.List;
 
 public interface CurrencyService {
+
     List<Currency> getCurrencies();
     Currency getCurrencyByCode(String code);
+    List<ExchangeRate> getExchangeRates();
 }
