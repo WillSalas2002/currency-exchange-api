01ede19478dc755a4e2f36140a1586df3a3ad68f
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-14 17:29:18 +0500

    Feat: Implemented doGet() method for endpoint: /exchangeRates

---
+++ src/main/java/currency_exchange_api/controller/ExchangeRatesServlet.java
@@ -0,0 +1,36 @@
+package currency_exchange_api.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import currency_exchange_api.dao.CurrencyDAO;
+import currency_exchange_api.dao.CurrencyDAOImpl;
+import currency_exchange_api.model.ExchangeRate;
+import currency_exchange_api.service.CurrencyService;
+import currency_exchange_api.service.CurrencyServiceImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/exchangeRates")
+public class ExchangeRatesServlet extends HttpServlet {
+    private CurrencyDAO currencyDAO = new CurrencyDAOImpl();
+    private CurrencyService currencyService = new CurrencyServiceImpl(currencyDAO);
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        List<ExchangeRate> exchangeRatesList = currencyService.getExchangeRates();
+
+        res.setContentType("application/json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(res.getOutputStream(), exchangeRatesList);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
