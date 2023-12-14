9f2cdc059584bf9b5637e9938279702aeeed6251
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-14 13:00:18 +0500

    Created CurrennciesServlet.java Class and implemented doGet() method for getting list of existing currencies

---
+++ src/main/java/currency_exchange_api/controller/CurrenciesServlet.java
@@ -0,0 +1,37 @@
+package currency_exchange_api.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import currency_exchange_api.dao.CurrencyDAO;
+import currency_exchange_api.dao.CurrencyDAOImpl;
+import currency_exchange_api.model.Currency;
+import currency_exchange_api.service.CurrencyService;
+import currency_exchange_api.service.CurrencyServiceImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/currencies")
+public class CurrenciesServlet extends HttpServlet {
+
+    private CurrencyDAO currencyDAO = new CurrencyDAOImpl();
+    private CurrencyService currencyService = new CurrencyServiceImpl(currencyDAO);
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        List<Currency> currencyList = currencyService.getCurrencies();
+
+        res.setContentType("application/json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(res.getOutputStream(), currencyList);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+}
