98f3cb2d85c6c11d0eb75b3cf55237752b876101
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-14 17:31:17 +0500

    Created ExchangeRateServlet.java class

---
+++ src/main/java/currency_exchange_api/controller/ExchangeRateServlet.java
@@ -0,0 +1,21 @@
+package currency_exchange_api.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ExchangeRateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
