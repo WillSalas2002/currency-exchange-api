ca0d448ec222d39de81fd16cb60ada798d2da9f8
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-14 16:57:31 +0500

    Implemented doGet() method for path: /currency/*

--- src/main/java/currency_exchange_api/controller/CurrencyServlet.java
+++ src/main/java/currency_exchange_api/controller/CurrencyServlet.java
@@ -1,17 +1,35 @@
 package currency_exchange_api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import currency_exchange_api.dao.CurrencyDAO;
+import currency_exchange_api.dao.CurrencyDAOImpl;
+import currency_exchange_api.model.Currency;
+import currency_exchange_api.service.CurrencyService;
+import currency_exchange_api.service.CurrencyServiceImpl;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
+@WebServlet("/currency/*")
 public class CurrencyServlet extends HttpServlet {
 
+    private CurrencyDAO currencyDAO = new CurrencyDAOImpl();
+    private CurrencyService currencyService = new CurrencyServiceImpl(currencyDAO);
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
+        String pathInfo = req.getPathInfo();
+        String code = pathInfo.substring(pathInfo.length() - 3).toUpperCase();
+
+        Currency currency = currencyService.getCurrencyByCode(code);
+
+        res.setContentType("application/json");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(res.getOutputStream(), currency);
     }
 
     @Override
