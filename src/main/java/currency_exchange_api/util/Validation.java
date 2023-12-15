7aded814e84a1b02b754afa87be6ae7654ee47d3
Ilkham Jumatov <batir2jumatov@gmail.com>
2023-12-15 23:44:16 +0500

    Created Validation.java class for checking is currency code valid

---
+++ src/main/java/currency_exchange_api/util/Validation.java
@@ -0,0 +1,23 @@
+package currency_exchange_api.util;
+
+public class Validation {
+
+    public static boolean isCodeValid(String code) {
+
+        if (code == null) {
+            return false;
+        }
+
+        if (code.length() != 3) {
+            return false;
+        }
+
+        for (int i = 0; i < code.length(); i++) {
+
+            if (code.charAt(i) < 'A' && code.charAt(i) > 'z') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
