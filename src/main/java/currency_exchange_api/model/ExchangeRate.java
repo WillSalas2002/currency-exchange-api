6b5f7f4b2bbb94135d19c01ad48cff320033dcf7
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-01-20 18:38:12 +0500

    Entities updated: implemented lombok

--- src/main/java/currency_exchange_api/model/ExchangeRate.java
+++ src/main/java/currency_exchange_api/model/ExchangeRate.java
@@ -1,48 +1,18 @@
 package currency_exchange_api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class ExchangeRate {
 
     private int id;
     private Currency baseCurrency;
     private Currency targetCurrency;
-    private double rate;
-
-    public ExchangeRate(int id, Currency baseCurrency, Currency targetCurrency, double rate) {
-        this.id = id;
-        this.baseCurrency = baseCurrency;
-        this.targetCurrency = targetCurrency;
-        this.rate = rate;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Currency getBaseCurrency() {
-        return baseCurrency;
-    }
-
-    public void setBaseCurrency(Currency baseCurrency) {
-        this.baseCurrency = baseCurrency;
-    }
-
-    public Currency getTargetCurrency() {
-        return targetCurrency;
-    }
-
-    public void setTargetCurrency(Currency targetCurrency) {
-        this.targetCurrency = targetCurrency;
-    }
-
-    public double getRate() {
-        return rate;
-    }
-
-    public void setRate(double rate) {
-        this.rate = rate;
-    }
+    private BigDecimal rate;
 }
