cf839c2037f25ded4ec911333d7f906645cdca13
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-01-20 16:50:50 +0500

    DB connection: changed the logic for getting db connection

--- src/main/java/currency_exchange_api/util/DatabaseUtil.java
+++ src/main/java/currency_exchange_api/util/DatabaseUtil.java
@@ -1,30 +1,35 @@
 package currency_exchange_api.util;
 
+import org.sqlite.SQLiteDataSource;
+
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DatabaseUtil {
+
+    private static final Logger LOGGER = Logger.getLogger(DatabaseUtil.class.getName());
+    private static final String DATABASE_URL = "jdbc:sqlite:C:/Program Files/JetBrains/java-projects/pet-projects/currency-exchange-api/src/main/resources/database.sqlite";
+    private static final SQLiteDataSource dataSource = new SQLiteDataSource();
+
+    static {
+        dataSource.setUrl(DATABASE_URL);
+    }
+
     public static Connection getConnection() {
-
-        Connection connection = null;
         try {
-
-            URL jdbcUrl = DatabaseUtil.class.getClassLoader().getResource("database.sqlite");
-            Class.forName("org.sqlite.JDBC");
-            String path = new File(jdbcUrl.toURI()).getAbsolutePath();
-            connection = DriverManager.getConnection("jdbc:sqlite:" + path);
-
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-
-        } catch (URISyntaxException e) {
-            e.getMessage();
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Failed to get database connection", e);
+            throw new RuntimeException("Failed to get database connection", e);
         }
+    }
 
-        return connection;
+    private DatabaseUtil() {
     }
 }
