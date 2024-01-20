38923740f23c027aa8dcd486c8858b6b0f90091d
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-01-20 23:02:19 +0500

    asdfasdfasfd

---
+++ src/main/java/currency_exchange_api/controller/EncodingFilter.java
@@ -0,0 +1,30 @@
+package currency_exchange_api.controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class EncodingFilterServlet implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
