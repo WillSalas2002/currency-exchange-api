9c0b56d76e2539ede1198d2ca551416ef9a51450
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-01-25 18:04:22 +0500

    Move sql scripts to field level in a class

--- src/main/java/currency/exchange/api/repository/JdbcCurrencyRepository.java
+++ src/main/java/currency/exchange/api/repository/JdbcCurrencyRepository.java
@@ -8,17 +8,18 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CurrencyDAO implements CurrencyRepository {
+public class JdbcCurrencyRepository implements CurrencyRepository {
+    private final static String FIND_ALL = "SELECT * FROM currencies";
+    private final static String FIND_BY_CODE = "SELECT * FROM currencies WHERE code = ?";
+    private final static String FIND_BY_ID = "SELECT * FROM currencies WHERE id = ?";
+    private final static String SQL_SAVE_CURRENCY = "INSERT INTO currencies (code, full_name, sign) VALUES (?,?,?)";
+
     @Override
     public List<Currency> findAll() throws SQLException {
-
-        String sql = "SELECT * FROM currencies";
         List<Currency> currencyList = new ArrayList<>();
-
         try (Connection connection = DatabaseUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL);
              ResultSet resultSet = preparedStatement.executeQuery()) {
-
             while (resultSet.next()) {
                 currencyList.add(toCurrency(resultSet));
             }
@@ -28,18 +29,11 @@ public class CurrencyDAO implements CurrencyRepository {
 
     @Override
     public Currency findByCode(String code) throws SQLException {
-
-        String sql = "SELECT * FROM currencies WHERE code = ?";
-
         try (Connection connection = DatabaseUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_CODE)) {
             preparedStatement.setString(1, code);
-
             ResultSet resultSet = preparedStatement.executeQuery();
-
             Currency currency = toCurrency(resultSet);
-
             if (currency.getId() == 0) {
                 throw new MissingCurrencyException("given currency is absent in database");
             }
@@ -49,38 +43,25 @@ public class CurrencyDAO implements CurrencyRepository {
 
     @Override
     public void save(Currency currency) throws SQLException {
-
-        String sql = "INSERT INTO currencies (code, full_name, sign) VALUES (?,?,?)";
-
         try (Connection connection = DatabaseUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-
+             PreparedStatement preparedStatement = connection.prepareStatement(SQL_SAVE_CURRENCY)) {
             preparedStatement.setString(1, currency.getCode());
             preparedStatement.setString(2, currency.getFullName());
             preparedStatement.setString(3, currency.getSign());
-
             preparedStatement.executeUpdate();
         }
     }
 
-    @Override
     public Currency findById(int id) throws SQLException {
-
-        String sql = "SELECT * FROM currencies WHERE id = ?";
-
         try (Connection connection = DatabaseUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-
+             PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID)) {
             preparedStatement.setInt(1, id);
-
             ResultSet resultSet = preparedStatement.executeQuery();
-
             return toCurrency(resultSet);
         }
     }
 
-    @Override
-    public Currency toCurrency(ResultSet resultSet) throws SQLException {
+    private Currency toCurrency(ResultSet resultSet) throws SQLException {
         return new Currency(
                 resultSet.getInt("id"),
                 resultSet.getString("code"),
