378db8a84ea3e2db29216f4f97461d198ba27752
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-01-25 18:06:57 +0500

    Write logic for converting ExchangeRateDto to ExchangeRate

--- src/main/java/currency/exchange/api/service/ExchangeRateService.java
+++ src/main/java/currency/exchange/api/service/ExchangeRateService.java
@@ -1,27 +1,36 @@
 package currency.exchange.api.service;
 
-import currency.exchange.api.dao.ExchangeRateRepository;
+import currency.exchange.api.dto.ExchangeRateDTO;
+import currency.exchange.api.repository.CurrencyRepository;
+import currency.exchange.api.repository.ExchangeRateRepository;
 import currency.exchange.api.exception.MissingCurrencyException;
 import currency.exchange.api.model.ExchangeRate;
+import currency.exchange.api.repository.JdbcCurrencyRepository;
+import currency.exchange.api.repository.JdbcExchangeRateRepository;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExchangeRateService {
-
-    private final ExchangeRateRepository exchangeRateRepository;
-
-    public ExchangeRateService(ExchangeRateRepository exchangeRateRepository) {
-        this.exchangeRateRepository = exchangeRateRepository;
-    }
+    private final ExchangeRateRepository exchangeRateRepository = new JdbcExchangeRateRepository();
+    private final CurrencyRepository currencyRepository = new JdbcCurrencyRepository();
 
     public List<ExchangeRate> findAll() throws SQLException {
-        return exchangeRateRepository.findAll();
+        List<ExchangeRate> exchangeRateList = new ArrayList<>();
+        List<ExchangeRateDTO> list = exchangeRateRepository.findAll();
+        for (ExchangeRateDTO item : list) {
+            exchangeRateList.add(toExchangeRate(item));
+        }
+        return exchangeRateList;
     }
 
     public ExchangeRate findByCurrencyCodes(String baseCurrencyCode, String targetCurrencyCode) throws SQLException {
-        return exchangeRateRepository.findByCurrencyCodes(baseCurrencyCode, targetCurrencyCode);
+        int baseCurrencyId = currencyRepository.findByCode(baseCurrencyCode).getId();
+        int targetCurrencyId = currencyRepository.findByCode(targetCurrencyCode).getId();
+        ExchangeRateDTO exchangeRateDTO = exchangeRateRepository.findByCurrencyCodes(baseCurrencyId, targetCurrencyId);
+        return toExchangeRate(exchangeRateDTO);
     }
 
     public void save(ExchangeRate exchangeRate) throws SQLException, MissingCurrencyException {
@@ -30,5 +39,14 @@ public class ExchangeRateService {
 
     public void update(int id, BigDecimal rate) throws SQLException {
         exchangeRateRepository.update(id, rate);
+    }
+
+    private ExchangeRate toExchangeRate(ExchangeRateDTO exchangeRateDTO) throws SQLException {
+        return new ExchangeRate(
+                exchangeRateDTO.getId(),
+                currencyRepository.findById(exchangeRateDTO.getBaseCurrencyId()),
+                currencyRepository.findById(exchangeRateDTO.getBaseCurrencyId()),
+                exchangeRateDTO.getRate()
+        );
     }
 }
